4c1da2a4e554ccf113b518f9cdc404da69660c6e
Guido Cruz Huanca <guido_cruz_huanca@outlook.com>
2020-10-27 21:20:53 -0400

    first commit backend project.

---
+++ src/test/java/com/iso/iso/GestionEntregasBackEndApplicationTests.java
@@ -0,0 +1,10 @@
+package com.iso.iso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEntregasBackEndApplicationTests {
+
+	
+}
